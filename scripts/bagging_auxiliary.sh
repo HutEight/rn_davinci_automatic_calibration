9b406e183d77b70a3aed179335ed984cb2a3b302
Will <sxh759@case.edu>
2018-08-09 18:36:05 -0400

    added new functions

--- scripts/bagging_auxiliary.sh
+++ scripts/bagging_auxiliary.sh
@@ -31,86 +31,47 @@ echo -e "${Bold_t}Da Vinci automatic calibration--Right (\033[0;32mgreen\033[1;3
 echo -e "\n${Bold_t}${Green_t}      Press any key to begin.${RS}${NC}\n\n"
 read -rsp $'' -n1 key;
 
+#############
+echo -e "\n  ${Bold_t}${Green_t}Please remove the tape!${NC}${RS}"
+echo -e "  After that, ${Bold_t}${Green_t}press any key${NC}${RS} to proceed.\n"
+read -rsp $'' -n1 key;
 
+
+rosrun cwru_davinci_playfile playback_jointspace /home/sxh759/catkin_ws/src/automatic_calibration/play/jsp/20180806_Qulaity_test_small_spheres_playfiles_PSM2/1a.jsp;
+sleep 10;
+echo -e "\n  ${Bold_t}Armed moved to Small spheres prepose${RS}\n"
+#############
+echo -e "\n  Recording Small Sphere Data 01..\n"
+echo -e "Reading playfile: 1_small_sphere.jsp"
+rosbag record --duration=200 -O  1_small_sphere.bag /polaris_sensor/targets &
+rosrun cwru_davinci_playfile playback_jointspace /home/sxh759/catkin_ws/src/automatic_calibration/play/jsp/20180806_Qulaity_test_small_spheres_playfiles_PSM2/1_small_sphere.jsp;
+sleep 200;
+echo -e "\n  ${Bold_t}Small Sphere Data 01 Recording Complete!${RS}\n"
 
 
 
 #############
-
-
-
-#############
-
-#############
-
-
-
-echo -e "\n  Recording Small Sphere Data 02..\n"
-echo -e "Reading playfile: 06_green_small_sphere_11cm.jsp"
-rosbag record --duration=265 -O  06_green_small_sphere_11cm.bag /polaris_sensor/targets &
-rosrun cwru_davinci_playfile playback_jointspace /home/sxh759/catkin_ws/src/automatic_calibration/play/jsp/20180216_prismatic_offsets_playfiles/06_green_small_sphere_11cm.jsp;
-sleep 270;
-echo -e "\n  ${Bold_t}Small Sphere Data 02 Recording Complete!${RS}\n"
-
-
-echo -e "\n  Recording Small Sphere Data 03..\n"
-echo -e "Reading playfile: 07_green_small_sphere_17cm.jsp"
-rosbag record --duration=265 -O  07_green_small_sphere_17cm.bag /polaris_sensor/targets &
-rosrun cwru_davinci_playfile playback_jointspace /home/sxh759/catkin_ws/src/automatic_calibration/play/jsp/20180216_prismatic_offsets_playfiles/07_green_small_sphere_17cm.jsp;
-sleep 270;
-echo -e "\n  ${Bold_t}Small Sphere Data 03 Recording Complete!${RS}\n"
-
-
-echo -e "\n  Recording Small Sphere Data 04..\n"
-echo -e "Reading playfile: 08_green_small_sphere_23cm.jsp"
-rosbag record --duration=265 -O  08_green_small_sphere_23cm.bag /polaris_sensor/targets &
-rosrun cwru_davinci_playfile playback_jointspace /home/sxh759/catkin_ws/src/automatic_calibration/play/jsp/20180216_prismatic_offsets_playfiles/08_green_small_sphere_23cm.jsp;
-sleep 270;
-echo -e "\n  ${Bold_t}Small Sphere Data 04 Recording Complete!${RS}\n"
-
 
 
 scrdir=$(rospack find rosbag_to_matlab);
 echo -e "\033[1;37m\nConverting rosbag data... \033[0m";
 
 
-bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/03_green_j1_arc_01.bag $PWD/outputs_conversion;
-bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/04_green_j2_arc_01.bag $PWD/outputs_conversion;
+bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/1_small_sphere.bag $PWD/outputs_conversion;
 
-bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/05_green_small_sphere_5cm.bag $PWD/outputs_conversion;
-bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/06_green_small_sphere_11cm.bag $PWD/outputs_conversion;
-bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/07_green_small_sphere_17cm.bag $PWD/outputs_conversion;
-bash /home/sxh759/catkin_ws/src/rosbag_to_matlab/scripts/bag_to_csv.sh $PWD/08_green_small_sphere_23cm.bag $PWD/outputs_conversion;
 
 echo -e "\t\033[1;37mConversion Complete!\033[0m";
 
 
 
-mv $PWD/outputs_conversion/03_green_j1_arc_01/polaris_sensor/targets.csv $PWD/03_green_j1_arc_01.csv;
-mv $PWD/outputs_conversion/04_green_j2_arc_01/polaris_sensor/targets.csv $PWD/04_green_j2_arc_01.csv;
-
-mv $PWD/outputs_conversion/05_green_small_sphere_5cm/polaris_sensor/targets.csv $PWD/05_green_small_sphere_5cm.csv;
-mv $PWD/outputs_conversion/06_green_small_sphere_11cm/polaris_sensor/targets.csv $PWD/06_green_small_sphere_11cm.csv;
-mv $PWD/outputs_conversion/07_green_small_sphere_17cm/polaris_sensor/targets.csv $PWD/07_green_small_sphere_17cm.csv;
-mv $PWD/outputs_conversion/08_green_small_sphere_23cm/polaris_sensor/targets.csv $PWD/08_green_small_sphere_23cm.csv;
+mv $PWD/outputs_conversion/1_small_sphere/polaris_sensor/targets.csv $PWD/1_small_sphere.csv;
 
 echo -e "\033[1;37m\nPreprocessing data... \033[0m";
 
 
 
-sed -i -e 's:/polaris_link,::g' $PWD/03_green_j1_arc_01.csv;
-sed -i -e "1d" $PWD/03_green_j1_arc_01.csv;
-sed -i -e 's:/polaris_link,::g' $PWD/04_green_j2_arc_01.csv;
-sed -i -e "1d" $PWD/04_green_j2_arc_01.csv;
-
-sed -i -e 's:/polaris_link,::g' $PWD/05_green_small_sphere_5cm.csv;
-sed -i -e "1d" $PWD/05_green_small_sphere_5cm.csv
-sed -i -e 's:/polaris_link,::g' $PWD/06_green_small_sphere_11cm.csv;
-sed -i -e "1d" $PWD/06_green_small_sphere_11cm.csv
-sed -i -e 's:/polaris_link,::g' $PWD/07_green_small_sphere_17cm.csv;
-sed -i -e "1d" $PWD/07_green_small_sphere_17cm.csv
-sed -i -e 's:/polaris_link,::g' $PWD/08_green_small_sphere_23cm.csv;
-sed -i -e "1d" $PWD/08_green_small_sphere_23cm.csv
+sed -i -e 's:/polaris_link,::g' $PWD/1_small_sphere.csv;
+sed -i -e "1d" $PWD/1_small_sphere.csv;
 
 echo -e "\t\033[1;37mComplete!\033[0m";
 
@@ -119,24 +80,14 @@ echo -e "\033[1;37m\nCleaning up.\033[0m";
 exedir=$(rospack find automatic_calibration);
 
 
-mv $PWD/03_green_j1_arc_01.csv $exedir/csv/03_green_j1_arc_01.csv;
-mv $PWD/04_green_j2_arc_01.csv $exedir/csv/04_green_j2_arc_01.csv;
+mv $PWD/1_small_sphere.csv $exedir/csv/1_small_sphere.csv;
 
-mv $PWD/05_green_small_sphere_5cm.csv $exedir/csv/05_green_small_sphere_5cm.csv;
-mv $PWD/06_green_small_sphere_11cm.csv $exedir/csv/06_green_small_sphere_11cm.csv;
-mv $PWD/07_green_small_sphere_17cm.csv $exedir/csv/07_green_small_sphere_17cm.csv;
-mv $PWD/08_green_small_sphere_23cm.csv $exedir/csv/08_green_small_sphere_23cm.csv;
 
 rm -rf $PWD/outputs_conversion/;
 
 
-rm 03_green_j1_arc_01.bag;
-rm 04_green_j2_arc_01.bag;
+rm 1_small_sphere.bag;
 
-rm 05_green_small_sphere_5cm.bag;
-rm 06_green_small_sphere_11cm.bag;
-rm 07_green_small_sphere_17cm.bag;
-rm 08_green_small_sphere_23cm.bag;
 
 echo -e "\033[1;34m\nCalibration data acquired.\033[0m";
 
